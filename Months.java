32ef886abfcb1f32fbc89a0c619a724b966e7a2e
AliciaMcHugh <50121552+AliciaMcHugh@users.noreply.github.com>
2019-04-29 23:09:47 +0100

    Add files via upload

---
+++ Months.java
@@ -0,0 +1,70 @@
+
+public class Months{
+	public static void main(String[] args){
+
+	int i;
+	String[] months = new String[12];
+
+	months[0] = "January";
+	months[1] = "Feburary";
+	months[2] = "March";
+	months[3] = "April";
+	months[4] = "May";
+	months[5] = "June";
+	months[6] = "July";
+	months[7] = "August";
+	months[8] = "September";
+	months[9] = "October";
+	months[10] = "November";
+	months[11] = "December";
+
+    if(months[0].contains("r"))
+	{
+		System.out.println(months[0]);
+    }
+    if(months[1].contains("r"))
+	{
+		System.out.println(months[1]);
+    }
+    if(months[2].contains("r"))
+	{
+		System.out.println(months[2]);
+    }
+    if(months[3].contains("r"))
+	{
+		System.out.println(months[3]);
+    }
+    if(months[4].contains("r"))
+	{
+		System.out.println(months[4]);
+    }
+    if(months[5].contains("r"))
+	{
+		System.out.println(months[5]);
+    }
+    if(months[6].contains("r"))
+	{
+		System.out.println(months[6]);
+    }
+    if(months[7].contains("r"))
+	{
+		System.out.println(months[7]);
+    }
+    if(months[8].contains("r"))
+	{
+		System.out.println(months[8]);
+    }
+    if(months[9].contains("r"))
+	{
+		System.out.println(months[9]);
+    }
+    if(months[10].contains("r"))
+	{
+		System.out.println(months[10]);
+    }
+    if(months[11].contains("r"))
+	{
+		System.out.println(months[11]);
+    }
+   }
+  }
